0c9e725c366ca23c5fdca4cc68323b07fd6df908
Dvard <edvard.busck-nielsen@edu.prakticum.fi>
2019-12-04 08:55:47 +0000

    Improved design

--- js/main.js
+++
@@ -1,6 +0,0 @@
-function toggleTab(index) {
-	document.querySelector('#tab-content-' + String(index)).style.display = 'block';
-	document.querySelector('#tab' + String(index) + '-btn').classList.toggle('active-tab');
-	document.querySelector('#tab-content-' + String((index === 1) ? 2 : 1)).style.display = 'none';
-	document.querySelector('#tab' + String((index === 1) ? 2 : 1) + '-btn').classList.toggle('active-tab');
-}
